c68f221760d05b0dd245d39b5c801089893005be
orkunakbas <orkunakbas1o@gmail.com>
2024-10-23 23:02:39 +0300

    hero section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link, Button } from "@nextui-org/react";
 
 const index = () => {
   return (
-    <div>
-      <h1>Giriş</h1>
+    <div className="container mx-auto text-center h-[32rem] flex justify-center items-center">
+      <div className="text">
+        <h1>Forget Everything You <br></br>Know About Insurance</h1>
+        <p className='mb-[75px]'>Instant everything. Incredible prices. Big heart.</p>
+        <Button
+          className='p-5 header-button text-white'
+          variant="shadow"
+        >
+          Check Our Prices
+        </Button>
       </div>
+      
+    </div>
   )
 }
 
